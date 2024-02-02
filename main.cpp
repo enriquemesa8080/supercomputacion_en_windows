eb405a52be080d8f7020e959e5e3ef6730a4d267
Enrique Mesa <33262138+enriquemesa8080@users.noreply.github.com>
2024-02-02 12:35:01 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
+    project_manager project_manager(27, 80, 90);
+    
     print("Supercomputacion en Windows\n");
     codes(27);
     flourish(200);
