3f0445f618de93f116609c7eb9a3bef56e1d2aed
Enrique Mesa <33262138+enriquemesa8080@users.noreply.github.com>
2024-02-02 12:28:03 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
-#include <codes.hpp>
-
+#include "codes.hpp"
+#include "flourish.cpp"
+#include "pull.cpp"
 
 using namespace std;
 
 int main(int argc, char *argv[]){
     print("Supercomputacion en Windows\n");
     codes(27);
+    flourish(200);
+    pull(270);
 }
