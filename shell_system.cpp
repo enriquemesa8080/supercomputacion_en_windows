0cc791983460797219ebc944a62dfc9fbd757b91
Enrique Mesa <33262138+enriquemesa8080@users.noreply.github.com>
2024-02-08 12:59:45 -0400

    Create shell_system.cpp

---
+++ shell_system.cpp
@@ -0,0 +1,13 @@
+/*
+It's pending to create a shell to handle the 
+supercomputing software and hardware.
+
+I am proposing create a unique ID for each
+computer connected as a way to ensure that
+i can adress any resource.
+
+I believe that in the programming is crucial to
+respect a format.
+
+0001.id/resource/technique
+*/
