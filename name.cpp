3112a75f96e39a483d5f57efac447c8ea073557f
Enrique Mesa <33262138+enriquemesa8080@users.noreply.github.com>
2024-02-08 18:22:29 -0400

    Create name.cpp

---
+++ name.cpp
@@ -0,0 +1,5 @@
+char name[] = "SUPERCOMPUTING IN WINDOWS\0";
+char host_name[] = "mainnode_v2_intelbased\0";
+char unique_id[] = "0001";
+
+//Identification von komputer im der netzwerk von superkomputen
